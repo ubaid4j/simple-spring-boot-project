af1f569fda0742a0931f19bc6437dc95ec8384a0
ubaid4j <ubaidshan007@gmail.com>
2024-04-01 18:17:18 +0500

    init

---
+++ src/test/java/dev/ubaid/ssbp/SimpleSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ubaid.ssbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
